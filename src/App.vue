52d74b6c3ef05912d84e23b42452114f67013494
Xavier Carol <xcarolr@gmail.com>
2023-04-04 18:34:40 +0200

    add basic footer

--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,28 @@ import { RouterView } from 'vue-router'
     <v-main>
       <RouterView />
     </v-main>
+    <v-footer>
+
+      <v-row justify="center" no-gutters>
+        <v-col class="text-center mt-4" cols="12">
+          <div>
+            <b>{{ $t('contact.title') }}</b><br>
+            C/ Pompeu Fabra, 50<br>
+            08922 Santa Coloma de Gramenet (Barcelona)<br>
+            Tel: 93 386 12 68 <br>
+            Fax: 93 386 74 42
+          </div>
+        </v-col>
+        <v-col class="text-center mt-4" cols="12">
+          <span>
+            © Copyright 2017. All Rights Reserved. <a href="http://www.teraac.com/aviso-legal/" target="_blank">Aviso
+              Legal</a>
+          </span>
+        </v-col>
+      </v-row>
+
+
+
+    </v-footer>
   </v-app>
 </template>
