521fac6df4361503e7a3c862cc0565dda8c97d47
Xavier Carol <xcarolr@gmail.com>
2023-04-04 18:13:38 +0200

    basic custom app

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-
-import { ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-import i18n from './plugins/i18n'
-
-const selected = ref('')
-
-const languages = ref([
-  { language: 'ca', title: 'Català' },
-  { language: 'es', title: 'Español' }
-])
-
-const { t } = useI18n({
-  inheritLocale: true,
-  useScope: 'local'
-})
-
-const changeLocale = (locale) => {
-  i18n.global.locale.value = locale
-}
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld :msg="t('welcome')" />
-
-      <select v-model="selected" @change="changeLocale(selected)">
-        <option disabled value="Select language"></option>
-        <option v-for="lang in languages" :key="lang.title" :value="lang.language">
-          {{ lang.title }}
-        </option>
-      </select>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        {{ $t('motto') }}
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
