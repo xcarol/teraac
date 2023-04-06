f1321a5e1ae37c9a9ec56e8b25f3c537a2d10d0f
Xavier Carol <xcarolr@gmail.com>
2023-04-06 17:04:53 +0200

    Basic landing page

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-col>
+        <v-img :src="banner"></v-img>
+      </v-col>
+      <v-col>
+        <v-card flat>
+          <v-card-title class="title">{{ $t('landing-page.title') }}</v-card-title>
+          <v-card-text class="text">{{ $t('landing-page.description') }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import banner from '../assets/Porta-500x250.jpg'
+
+export default {
+  name: 'LandingPage',
+
+  data: () => ({
+    banner,
+  })
+}
+</script>
