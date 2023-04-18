eaa69859b04757554c1034f06b53f431e165f7a6
Xavier Carol <xcarolr@gmail.com>
2023-04-18 17:46:11 +0200

    use dynamic routing for the drawer menu

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -8,7 +8,7 @@
       <p class="company">TERAAC</p>
     </template>
     <template v-slot:prepend>
-      <v-img @click="gotoMain()" :src="logoImg" width="100" class="logo"></v-img>
+      <v-img @click="goto('home')" :src="logoImg" width="100" class="logo"></v-img>
     </template>
   </v-app-bar>
   <v-app-bar height="45">
@@ -16,9 +16,9 @@
       <v-row>
         <v-spacer />
         <v-col class="flex-grow-0 flex-shrink-1 d-none d-sm-block">
-          <div class="d-inline-block align-center" v-for="(item, index) in items" :key="index"> <router-link class="menu-link" :to="item.to">{{
-            $t(item.menu) }}</router-link></div>
-          <!-- <router-link class="menu-link" to="services">{{ $t('menu.services') }}</router-link> -->
+          <div class="d-inline-block align-center" v-for="(item, index) in items" :key="index">
+            <router-link class="menu-link" :to="item.to">{{ $t(item.menu) }}</router-link>
+          </div>
         </v-col>
       </v-row>
     </v-app-bar-title>
@@ -28,8 +28,8 @@
   </v-app-bar>
   <v-navigation-drawer location="right" v-model="drawer" temporary>
     <v-list>
-      <v-list-item v-for="item in items" :key="item.menu">
-        <router-link class="menu-link" :to="item.to">{{ $t(item.menu) }}</router-link>
+      <v-list-item class="menu-link" v-for="item in items" :key="item.to" :title="$t(item.menu)"
+        @click.stop="goto(item.to)">
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
@@ -52,8 +52,9 @@ export default defineComponent({
   }),
 
   methods: {
-    gotoMain() {
-      this.$router.push({ name: 'home' });
+    goto(page) {
+      this.$router.push({ name: page });
+      this.drawer = false;
     },
   },
 });
