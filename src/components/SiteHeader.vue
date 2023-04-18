e353efd67d3187ded8ab1775e300812198996532
Xavier Carol <xcarolr@gmail.com>
2023-04-18 17:32:20 +0200

    adapt menu

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,26 +1,38 @@
 <template>
   <v-app-bar flat height="25"></v-app-bar>
   <v-app-bar flat color="#003253" height="60" class="main-app-bar">
-      <v-app-bar-title class="motto">
-        {{ $t('motto') }}
-      </v-app-bar-title>
-      <template v-slot:append>
-        <p class="company">TERAAC</p>
-      </template>
-      <template v-slot:prepend>
-        <v-img @click="gotoMain()" :src="logoImg" width="100" class="logo"></v-img>
-      </template>
-    </v-app-bar>
-    <v-app-bar height="45">
-      <v-app-bar-title>
-        <v-row>
-          <v-col class="text-right" style="margin-inline-end: 16px;">
-            <router-link class="menu-link" to="about">{{ $t('menu.about-us') }}</router-link>
-            <router-link class="menu-link" to="services">{{ $t('menu.services') }}</router-link>
-          </v-col>
-        </v-row>
-      </v-app-bar-title>
-    </v-app-bar>
+    <v-app-bar-title class="motto">
+      {{ $t('motto') }}
+    </v-app-bar-title>
+    <template v-slot:append>
+      <p class="company">TERAAC</p>
+    </template>
+    <template v-slot:prepend>
+      <v-img @click="gotoMain()" :src="logoImg" width="100" class="logo"></v-img>
+    </template>
+  </v-app-bar>
+  <v-app-bar height="45">
+    <v-app-bar-title>
+      <v-row>
+        <v-spacer />
+        <v-col class="flex-grow-0 flex-shrink-1 d-none d-sm-block">
+          <div class="d-inline-block align-center" v-for="(item, index) in items" :key="index"> <router-link class="menu-link" :to="item.to">{{
+            $t(item.menu) }}</router-link></div>
+          <!-- <router-link class="menu-link" to="services">{{ $t('menu.services') }}</router-link> -->
+        </v-col>
+      </v-row>
+    </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn class="d-xs-block d-sm-none" icon="mdi-menu" @click.stop="drawer = !drawer"></v-btn>
+    </template>
+  </v-app-bar>
+  <v-navigation-drawer location="right" v-model="drawer" temporary>
+    <v-list>
+      <v-list-item v-for="item in items" :key="item.menu">
+        <router-link class="menu-link" :to="item.to">{{ $t(item.menu) }}</router-link>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -32,6 +44,11 @@ export default defineComponent({
 
   data: () => ({
     logoImg: Logo,
+    drawer: false,
+    items: [
+      { to: 'about', menu: 'menu.about-us' },
+      { to: 'services', menu: 'menu.services' },
+    ],
   }),
 
   methods: {
