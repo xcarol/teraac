a87f85b2b0b33a937d729b355c6d576d65fb743b
Xavier Carol <xcarolr@gmail.com>
2023-04-24 15:59:03 +0200

    fix texts errors

--- src/components/AboutUsCa.vue
+++ src/components/AboutUsCa.vue
@@ -1,50 +1,48 @@
 <template>
   <v-card-text>
     <p>
-      Teraac és una empresa familiar de segona generació. Fundada l'any 1965 per Andreu Carol, ha
-      anat creixent amb el temps. Als seus inicis la reparació d'automòbils es basava principalment
-      en la destresa manual i el enginy. Actualment els cotxes han evolucionat fins a l'extrem de
-      portar incorporats ordinadors de control.
+      Teraac és una empresa familiar de segona generació. Fundada l'any 1965 per Andreu Carol, ha anat creixent amb el
+      temps. Als seus inicis la reparació d'automòbils es basava principalment en la destresa manual i el enginy.
+      Actualment els cotxes han evolucionat fins a l'extrem de portar incorporats ordinadors de control.
     </p><br />
     <p>
-      Quan Teraac va iniciar la seva tasca de servei ho va fer en un local petit i amb l'Andreu com
-      a unic treballador, eren temps dificils i de molta feina.
+      Quan Teraac va iniciar la seva tasca de servei ho va fer en un local petit i amb l'Andreu com a únic treballador,
+      eren temps difícils i de molta feina.
     </p><br />
     <p>
-      Mes tard es van anar incorporant treballadors i l'empresa es va dotar d'unes instal·lacions
-      més amplies i mes adequades al nou volum de treball que l'empresa havia de servir.
+      Més tard, es van anar incorporant treballadors i l'empresa es va dotar d'unes instal·lacions més àmplies i més
+      adequades al nou volum de treball que havia de servir.
     </p><br />
     <p>
-      Actualment Teraac es caracteritza per ser una empresa que aquesta proveïda dels darrers
-      equipaments en matèria de diagnòstic i reparació de les avaries de l'automòbil, amb un
-      personal altament qualificat que continua formacio, sempre a disposicio i per donar el millor
-      servei al client.
+      Actualment, Teraac es caracteritza per ser una empresa que està proveïda dels darrers equipaments en matèria de
+      diagnòstic i reparació de les avaries de l'automòbil, amb un personal altament qualificat que continua reben formació,
+      sempre a disposició i per donar el millor servei al client.
     </p><br />
   </v-card-text>
   <v-card-title>
     Bosch Car Service
   </v-card-title>
   <v-card-text>
-    <p>Els conductors ho tenen clar. Pensar en Bosch Car Service és garantir qualitat per al cotxe al millor preu,
-      sigui de la marca que sigui, en diagnosi, manteniment, reparacions, o en la revisió del cotxe nou sense perdre la
-      garantia dorigen. Bosch Car Service. Qualitat, Preu i tots els Serveis”.
+    <p>
+      Els conductors ho tenen clar. Pensar en Bosch Car Service és garantir la qualitat per al cotxe al millor preu, sigui de
+      la marca que sigui, en diagnosi, manteniment, reparacions, o en la revisió del cotxe nou sense perdre la garantia
+      d'origen. Bosch Car Service "Qualitat, Preu i tots els Serveis”.
     </p><br />
     <p>
       La Xarxa oficial Bosch Car Service (BCS), amb més de 600 tallers repartits per tot Espanya, ofereix un servei
-      integral per a qualsevol marca d´automòbil. Perquè vostè exigeix per al seu cotxe un servei professional, de
-      qualitat,
-      confiança ia un preu raonable, els tallers Bosch Car Service (BCS) fan des del manteniment més senzill
-      fins a la reparació més complexa ja que compten amb els millors mecànics per garantir la reparació i
-      manteniment del vostre automòbil.
+      integral per a qualsevol marca d'automòbil. Perquè vostè exigeix per al seu cotxe un servei professional, de
+      qualitat, confiança i a un preu raonable, els tallers Bosch Car Service (BCS) fan des del manteniment més senzill
+      fins a la reparació més complexa, ja que compten amb els millors mecànics per garantir la reparació i manteniment del
+      vostre automòbil.
     </p><br />
     <p>
-      Els tallers Bosch Car Service (BCS) reben suport tècnic, empresarial i comercial per part de Bosch, líder
-      mundial com a fabricant de peces de l'automòbil.
+      Els tallers Bosch Car Service (BCS) reben suport tècnic, empresarial i comercial per part de Bosch, líder mundial
+      com a fabricant de peces de l'automòbil.
     </p><br />
     <p>
-      Tot per al vostre automòbil: revisions, desgast d'elements, pre-itv, revisions de nivell, manteniments dins i
-      fora del període de garantia, electricitat i electrònica, mecànica en general, reparacions complexes, injecció
-      dièsel i benzina, frens, ABS…
+      Tot per al vostre automòbil: revisions, desgast d'elements, pre-itv, revisions de nivell, manteniments dins i fora
+      del període de garantia, electricitat i electrònica, mecànica en general, reparacions complexes, injecció dièsel i
+      benzina, frens, ABS…
     </p><br />
     <p>Xarxa Oficial de tallers Bosch Car Service.</p><br />
     <p><b>Qualitat, Preu i tots els serveis</b></p><br />
--- src/components/AboutUsEs.vue
+++ src/components/AboutUsEs.vue
@@ -1,37 +1,36 @@
 <template>
   <v-card-text>
     <p>
-      Teraac es una empresa familiar de segunda generación. Fundada el año 1965 por Andreu Carol, ha
-      ido creciente con el tiempo. En sus inicios la reparación de automobiles se basaba
-      principalmente en la destreza manual y el ingenio. Actualmente los coches han evolucionado
-      hasta el extremo de traer incorporado ordenadores de control.
+      Teraac es una empresa familiar de segunda generación. Fundada el año 1965 por Andreu Carol, ha ido creciendo con el
+      tiempo. En sus inicios la reparación de automóviles se basaba principalmente en la destreza manual y el ingenio.
+      Actualmente los coches han evolucionado hasta el extremo de traer incorporado ordenadores de control.
     </p><br />
     <p>
-      Cuando Teraac inició su tarea de servicio lo hizo en un local pequeño y con Andreu como unic
-      trabajador, eran tiempo dificils y de mucho trabajo.
+      Cuando Teraac inició su tarea de servicio lo hizo en un local pequeño y con Andreu como único trabajador, eran
+      tiempos difíciles y de mucho trabajo.
     </p><br />
     <p>
-      Mes tarde se fueron incorporando trabajadores y la empresa se dotó de unas instalaciones mas
-      amplías y mes adecuades al nuevo volumen de trabajo que la empresa debía servir.
+      Más tarde, se fueron incorporando trabajadores y la empresa se dotó de unas instalaciones más amplías y más adecuadas
+      al nuevo volumen de trabajo que debía servir.
     </p><br />
     <p>
-      Actualmente Teraac se caracteriza por ser una empresa que esta provista de los ultimos
-      equipamientos en materia de diagnostico y reparación de las averias del automóvil, con un
-      personal altamente cualificado que esta continúa formacio, siempre a disposicio y por dar el
-      mejor servicio al cliente.
+      Actualmente, Teraac se caracteriza por ser una empresa que está provista de los últimos equipamientos en materia de
+      diagnóstico y reparación de las averías del automóvil, con un personal altamente cualificado que está en continúa
+      formación, siempre a disposición y por dar el mejor servicio al cliente.
     </p><br />
   </v-card-text>
   <v-card-title>
     Bosch Car Service
   </v-card-title>
   <v-card-text>
-    <p>Los conductores lo tienen claro. Pensar en Bosch Car Service es garantizar calidad para su coche al mejor precio,
-      sea de la marca que sea, en diagnosis, mantenimiento, reparaciones, o en la revisión de su coche nuevo sin perder la
-      garantía de origen. Bosch Car Service. Calidad, Precio, y todos los Servicios”.
+    <p>
+      Los conductores lo tienen claro. Pensar en Bosch Car Service es garantizar calidad para su coche al mejor precio,
+      sea de la marca que sea, en diagnóstico, mantenimiento, reparaciones, o en la revisión de su coche nuevo sin perder
+      la garantía de origen. Bosch Car Service “Calidad, Precio, y todos los Servicios”.
     </p><br />
     <p>
       La Red oficial Bosch Car Service (BCS), con más de 600 talleres repartidos por toda España, ofrece un servicio
-      integral para cualquier marca de automóvil. Porque usted exige para su coche un servicio profesional, de calidad,
+      integral para cualquier marca de automóviles. Porque usted exige para su coche un servicio profesional, de calidad,
       confianza y a un precio razonable, los talleres Bosch Car Service (BCS) realizan desde el mantenimiento más sencillo
       hasta la reparación más compleja ya que cuentan con los mejores mecánicos para garantizar la reparación y
       mantenimiento de su automóvil.
@@ -43,7 +42,7 @@
     <p>
       Todo para su automóvil: revisiones, desgaste de elementos, pre-itv, revisiones de nivel, mantenimientos dentro y
       fuera del periodo de garantía, electricidad y electrónica, mecánica en general, reparaciones complejas, inyección
-      diesel y gasolina, frenos, ABS…
+      diésel y gasolina, frenos, ABS…
     </p><br />
     <p>Red Oficial de talleres Bosch Car Service.</p><br />
     <p><b>Calidad, Precio y todos los Servicios</b></p><br />
