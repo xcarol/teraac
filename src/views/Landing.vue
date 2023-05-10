52f9a8fd099b22c2812f147f0203866f348b2851
Xavier Carol <xcarolr@gmail.com>
2023-05-10 19:15:50 +0200

    fix mixed styles names

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -6,8 +6,8 @@
       </v-col>
       <v-col cols="12" sm="6">
         <v-card flat>
-          <v-card-title class="brand-title">{{ $t('landing-page.title') }}</v-card-title>
-          <v-card-text class="brand-text">{{ $t('landing-page.description') }}</v-card-text>
+          <v-card-title class="company-title">{{ $t('landing-page.title') }}</v-card-title>
+          <v-card-text class="company-title">{{ $t('landing-page.description') }}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
