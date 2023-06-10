a433fa222b9f022e06f114dcb1dc338da5cfb2ca
Xavier Carol <xcarolr@gmail.com>
2023-06-10 18:53:13 +0200

    add head plugin to modify html head tags

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import i18n from './plugins/i18n';
 import vuetify from './plugins/vuetify';
+import head from './plugins/head';
 
 import loadFonts from './plugins/webfontloader';
 
@@ -15,5 +16,6 @@ app.use(router);
 app.use(i18n);
 app.use(vuetify);
 app.use(VueCookies);
+app.use(head, { $t: app.config.globalProperties.$t });
 
 app.mount('#app');
